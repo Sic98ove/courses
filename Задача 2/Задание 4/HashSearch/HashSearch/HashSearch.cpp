a16d97688e668b97aabe6fd56e0a05885aa32d37
Sic98ove <83631579+Sic98ove@users.noreply.github.com>
2021-05-04 09:46:49 +0300

    Add files via upload

---
+++ Задача 2/Задание 4/HashSearch/HashSearch/HashSearch.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <set>
+#include <time.h>
+
+int main()
+{
+	srand(time(NULL));
+	setlocale(LC_ALL, "russian");
+
+	std::cout << "Введите количество элементов массива ";
+	int count;
+	std::cin >> count;
+	std::cout << std::endl;
+
+	int *a = new int[count];
+	std::set<int> search;					//множество, содержащее элементы с помощью хэша
+
+	for (size_t i = 0; i < count; i++)
+	{
+		a[i] = rand() % 100 + 1;
+		//std::cout << a[i] << std::endl;
+		search.insert(a[i]);				//Каждый новый элемент добавляем в множество
+	}
+
+	std::cout << std::endl << "Введите значение для поиска ";
+	int value;
+	std::cin >> value;
+	std::cout << std::endl;
+
+	if (search.find(value) != search.end())	//Вызываем find и проверяем, что он указывает не на конец коллекции, если указывает на конец, значит элемент не найден
+	{
+		std::cout << "Элемент найден";
+	}
+	else
+	{
+		std::cout << "Элемент не найден";
+	}
+}
---
+++ Задача 2/Задание 4/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        
+        public int[] intArray;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Создать одномерный массив длинной n заполненный случайными данными.
+        /// </summary>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public int[] CreateArray(int n)
+        {
+            var rand = new Random();
+            int[] arr = new Int32[n];
+
+            for (int i = 0; i < n; i++)
+            {
+
+                arr[i] = rand.Next(101);
+            }
+
+            return arr;
+        }
+
+        public void DisplayArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            intArray = new int[Int32.Parse(textBox1.Text)];
+
+            intArray = CreateArray(Int32.Parse(textBox1.Text));
+
+            DisplayArray(intArray);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] tmpSource = new byte[intArray.Length];
+            byte[] tmpHash;
+
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                tmpSource[i] = Convert.ToByte(intArray[i]);
+            }
+            tmpHash = new MD5CryptoServiceProvider().ComputeHash(tmpSource);
+            Console.WriteLine(tmpSource.ToString());
+            for (int i = 0; i < tmpHash.Length; i++)
+            {
+                Console.WriteLine(tmpHash[i]);
+            }
+
+            int tempValue = Int32.Parse(textBox2.Text);
+
+            byte[] temp = new byte[1]; 
+            temp[0] = Convert.ToByte(tempValue);
+
+            byte[] valueHash = new MD5CryptoServiceProvider().ComputeHash(temp);
+
+            bool flag = false;
+            
+            for (int i = 0; i < tmpHash.Length; i++)
+            {
+                if (tmpHash[i] == valueHash[0])
+                {
+                    flag = true;
+                    break;
+                    
+                }
+            }
+
+            if (flag)
+            {
+                MessageBox.Show("Искомое значение есть в массиве");
+            }
+            else
+            {
+                MessageBox.Show("Искомого значения нет в массиве");
+            }
+
+        }
+    }
+}
